d7bdeb5e9184200c61f4b8d569dab11327b0097f
MohamadAliAwarkeh <mohamad.awarkeh@gmail.com>
2018-10-30 10:32:00 +0000

    More shit done

---
+++ Chromaneers REWORK/Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName ="Scriptable Objects/Weapon Stats")]
+public class Weapon : ScriptableObject {
+
+    public string wepName;
+
+    public int damage;
+    public int fireRate;
+    public int bulletSpread;
+    public int range;
+
+    public ParticleSystem trailParticle;
+    public ParticleSystem explosionParticle;
+}
---
+++ Chromaneers REWORK/Assets/WeaponPopUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPopUI : MonoBehaviour {
+
+    public Weapon weapon;
+
+    public Text nameText;
+
+    public Slider damageSlider;
+    public Slider fireRateSlider;
+    public Slider bulletSpreadSlider;
+    public Slider rangeSlider;
+
+	void Start () {
+        nameText.text = weapon.wepName;
+
+        damageSlider.value = weapon.damage;
+        fireRateSlider.value = weapon.fireRate;
+        bulletSpreadSlider.value = weapon.bulletSpread;
+        rangeSlider.value = weapon.range;
+	}
+	
+
+}
